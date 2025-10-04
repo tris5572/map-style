67765e6284c9fcdb2c1c2713648af00015b4f7ff
tris <68403260+tris5572@users.noreply.github.com>
2025-10-04 14:10:47 +0900

    feat: 地図を表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,32 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Map, {
+  FullscreenControl,
+  GeolocateControl,
+  NavigationControl,
+  ScaleControl,
+  type StyleSpecification,
+} from "react-map-gl/maplibre";
+import "maplibre-gl/dist/maplibre-gl.css";
 import "./App.css";
+import * as mapStyleJson from "./map-styles/light.json";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          onClick={() => {
-            setCount((count) => count + 1);
-          }}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <div id="map">
+      <Map
+        initialViewState={{
+          latitude: 36,
+          longitude: 138,
+          zoom: 7,
+        }}
+        mapStyle={mapStyleJson as StyleSpecification}
+        attributionControl={false}
+      >
+        <ScaleControl />
+        <NavigationControl />
+        <FullscreenControl />
+        <GeolocateControl />
+      </Map>
+    </div>
   );
 }
 
