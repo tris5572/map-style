a6cdff7b91763e88916201fc4f203528b5c6bc00
tris <68403260+tris5572@users.noreply.github.com>
2025-10-15 05:52:33 +0900

    refactor: スタイルファイルを各フォルダへ移動

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Map, {
 } from "react-map-gl/maplibre";
 import "maplibre-gl/dist/maplibre-gl.css";
 import "./App.css";
-import * as mapStyleJson from "../public/styles/dark.json";
+import * as mapStyleJson from "../public/dark/style.json";
 
 export function App() {
   return (
