e68280745ba3f7f8a4e38307f7f30a9b8d9862ed
tris <68403260+tris5572@users.noreply.github.com>
2025-10-15 20:37:26 +0900

    feat: 地図スタイルの切り替えを実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useState } from "react";
 import Map, {
   FullscreenControl,
   GeolocateControl,
@@ -7,9 +8,22 @@ import Map, {
 } from "react-map-gl/maplibre";
 import "maplibre-gl/dist/maplibre-gl.css";
 import "./App.css";
-import * as mapStyleJson from "../public/dark/style.json";
+import * as darkStyleJson from "../public/dark/style.json";
+import * as lightStyleJson from "../public/light/style.json";
+
+/** 地図スタイルのデータのリスト */
+const STYLES = [
+  { name: "Light", json: lightStyleJson as StyleSpecification },
+  { name: "Dark", json: darkStyleJson as StyleSpecification },
+];
 
 export function App() {
+  const [selectedStyleIndex, setSelectedStyleIndex] = useState<number>(0);
+
+  const handleStyleChange = useCallback((index: number) => {
+    setSelectedStyleIndex(index);
+  }, []);
+
   return (
     <div id="map">
       <Map
@@ -18,7 +32,7 @@ export function App() {
           longitude: 138,
           zoom: 7,
         }}
-        mapStyle={mapStyleJson as StyleSpecification}
+        mapStyle={STYLES[selectedStyleIndex].json}
         attributionControl={false}
       >
         <ScaleControl />
@@ -26,6 +40,35 @@ export function App() {
         <FullscreenControl />
         <GeolocateControl />
       </Map>
+      <StyleSwitcher styleIndex={selectedStyleIndex} handleStyleChange={handleStyleChange} />
+    </div>
+  );
+}
+
+/**
+ * 地図スタイルの切替ボタンを表示するコンポーネント
+ */
+function StyleSwitcher(props: { styleIndex: number; handleStyleChange: (index: number) => void }) {
+  const handleClick = useCallback((index: number) => {
+    props.handleStyleChange(index);
+  }, []);
+
+  return (
+    <div id="style-switcher">
+      <div id="button-wrapper">
+        {STYLES.map((st, i) => (
+          <button
+            key={st.name}
+            type="submit"
+            className={i === props.styleIndex ? "selected" : undefined}
+            onClick={() => {
+              handleClick(i);
+            }}
+          >
+            {st.name}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
